03d452e43dbcfc898485a5303402a47860ff678f
FIS40063783 <nfisher2670@gmail.com>
2022-05-29 22:44:50 +0100

    grab lsp config

---
+++ lua/packages.lua
@@ -0,0 +1,14 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require('packer').startup(function(use)
+	use 'neovim/nvim-lspconfig'	-- configurations for language server protocol
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
