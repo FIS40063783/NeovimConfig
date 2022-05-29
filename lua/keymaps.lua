11db71e5c2d323d759e383ddd2397776f686a097
FIS40063783 <nfisher2670@gmail.com>
2022-05-29 22:32:37 +0100

    comment out split navigation

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,6 +2,13 @@
 vim.api.nvim_set_keymap('n', '<Space>', "<nop>", {noremap = true, silent = true})
 vim.g.mapleader = '<Space>'
 
+--[[ Disable arrow key movement
+vim.api.nvim_set_keymap({'i', 'n', 'v'}, '<Up>', '<nop>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap({'i', 'n', 'v'}, '<Down>', '<nop>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap({'i', 'n', 'v'}, '<Left>', '<nop>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap({'i', 'n', 'v'}, '<Right>', '<nop>', {noremap = true, silent = true})
+--]]
+
 -- Easier split navigation
 vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {noremap = true, silent = true})
