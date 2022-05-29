a181406f17254950aa220a23bfff63f101544e39
FIS40063783 <nfisher2670@gmail.com>
2022-05-29 19:18:18 +0100

    move window navigation to lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,2 +1,9 @@
+-- Setting leader key
 vim.api.nvim_set_keymap('n', '<Space>', "<nop>", {noremap = true, silent = true})
 vim.g.mapleader = '<Space>'
+
+-- Easier split navigation
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {noremap = true, silent = true})
