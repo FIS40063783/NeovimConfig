99284d2991b1fdaaea43b1ad9dae6a0b9ede57f3
FIS40063783 <nfisher2670@gmail.com>
2022-05-31 22:18:37 +0100

    remove cmp-cmdline

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -8,10 +8,6 @@
         require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
       end,
     },
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
     mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -42,14 +38,4 @@
     sources = {
       { name = 'buffer' }
     }
-  })
-
-  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
-      { name = 'cmdline' }
-    })
   })
